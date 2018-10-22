b2692b027517617431cfafe8325740af593bd0c2
Fernando Diaz <fer9h7@gmail.com>
2018-10-22 12:43:53 -0600

    adding dates list component

---
+++ client/src/app/layout/dates-list/dates-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dates-list',
+  templateUrl: './dates-list.component.html',
+  styleUrls: ['./dates-list.component.scss']
+})
+export class DatesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
